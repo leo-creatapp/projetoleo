c04507f3253c68b9f2ccd45ea327590d1d755a35
leo-creatapp <69485129+leo-creatapp@users.noreply.github.com>
2020-08-10 19:11:31 -0300

    Add files via upload

---
+++ www/js/index.js
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+
+
+/////////////////camera//////////////
+
+
+var app = {
+
+    capture: function () {
+
+
+
+        navigator.camera.getPicture(onSuccess, onFail, {
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL,
+            salvarToPhotoAlbum: true,
+            cameraDirection: Camera.Direction.FRONT,
+            targetHeight: 1000,
+            TargetWidth: 1000
+
+
+        });
+
+        function onSuccess(imageData) {
+            var image = document.getElementById('minhaimagem');
+            image.style.display = "block";
+            image.src = "data:image/jpeg;base64," + imageData;
+
+
+        }
+
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        }
+
+
+    }
+
+
+
+
+};
